c581b25b87d534bb59835e29f683251b15a87e8d
Taylor Tellman <taylortellman@gmail.com>
2018-05-18 21:38:23 +0000

    automatically backed up by learn

--- objects.js
+++
@@ -1,8 +0,0 @@
-var playlist = {artist: "song"};
-function updatePlaylist(playlist, artistName, songTitle){
-    return Object.assign( playlist, {[artistName]: songTitle})
-}
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.Slowdive
-  return playlist
-}
